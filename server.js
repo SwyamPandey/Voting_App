7bbc3838a909e8d7e0bb61eade8e73ec3a59b377
Swyam Pandey <swyampandey955950@gmail.com>
2025-07-18 21:39:31 +0530

    cors update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 app.use(express.json()); // Modern alternative to body-parser
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
